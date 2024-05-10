d895562093046e725f56ca46188aa75b2cf23269
pepperrostami <pepper.rostami@gmail.com>
2024-05-10 18:37:34 -0400

    updated grid to 30 cards, changed time limit to 90 seconds, changed chances to 30

--- js/script.js
+++ js/script.js
@@ -1,39 +1,44 @@
-const SOURCE_CARDS = [
+const artObjects = [
+    { img: 'img/02.Head_of_Gudea_(Metropolitan_Museum_of_Art).png', matched: false },
     { img: 'img/03.Statue_of_Gudea,_Metropolitan_Museum_of_Art.png', matched: false },
+    { img: 'img/04.Human-headed_bison_Neo-Sumerian_circa_2080_BCE.png', matched: false},
     { img: 'img/05.Ringstone_MET_DT9196.png', matched: false },
     { img: 'img/07.Scepter_(MET_collection).png', matched: false },
+    { img: 'img/08.Whip_Handle_in_the_Shape_of_a_Horse_1390-1353_BCE.png', matched: false },
     { img: 'img/09.Sarmatian_cup_with_animal_handle_(1st_century_CE,_reproduction).png', matched: false },
+    { img: 'img/10.Old_Assyrian_drinking_vessel_Kültepe.png', matched: false},
     { img: 'img/11.Etruscan_bronze_funerary_urn_with_Scythian_mounted_archer,_mid-5th_century_BCE.png', matched: false },
     { img: 'img/12.Terracotta_skyphos.png', matched: false },
     { img: 'img/13.AI-Restored_Head_of_an_Oba,_Benin_Bronze,_1550.png', matched: false },
+    { img: 'img/14.Cup_with_a_Poem_on_Wine._Ibn_Sukkara_al-Hashimi_(d._995–6_CE)._Buyid_dynasty._Iran.png', matched: false },
     { img: 'img/15.Pyxis_attique_à_figures_rouges_MET.png', matched: false },
     { img: 'img/17.Kushano-Sasanian_footed_cup_with_medallion_3rd-4th_century_CE_Bactria_Metropolitan_Museum_of_Art.png', matched: false },
     { img: 'img/18.Terret_(Rein_Guide)_Celtic_or_Roman_The_Metropolitan_Museum_of_Art.png', matched: false }
   ];  
-  const CARD_BACK = 'img/black-square-1000x750mm.png';
+const objectCover = 'img/black-square-1000x750mm.png';
   
-  const backgroundMusic = document.getElementById('background-music');
-  const startSound = new Audio('mp3/FTF Start.wav');
-  const clickSound = new Audio('mp3/FTF Ding.wav');
-  const matchSound = new Audio('mp3/FTF Correct.wav');
-  const loseSound = new Audio('mp3/FTF Lose louder.wav')
-  const winSound = new Audio('mp3/FTF Win louder.wav');
-  const wrongSound = new Audio('mp3/FTF Wrong.wav');
+const backgroundMusic = document.getElementById('background-music');
+const startSound = new Audio('mp3/FTF Start.wav');
+const clickSound = new Audio('mp3/FTF Ding.wav');
+const matchSound = new Audio('mp3/FTF Correct.wav');
+const loseSound = new Audio('mp3/FTF Lose louder.wav')
+const winSound = new Audio('mp3/FTF Win louder.wav');
+const wrongSound = new Audio('mp3/FTF Wrong.wav');
   
-  let startTime, cards, selectedCard, ignoreClicks, matches, seconds, chances;
-  let numMoves = 0;
-  let totalMatches = 0;
-  let totalMatchTime = 0;
+let cards, selectedCard, ignoreClicks, matches, seconds, chances;
+let numMoves = 0;
+let totalMatches = 0;
+let totalMatchTime = 0;
   
-  const chancesEl = document.querySelector('h2');
-  const timerEl = document.getElementById('counter');
-  const introModal = document.getElementById('intro-modal-container');
-  const loseModal = document.getElementById('lose-modal-container');
-  const winModal = document.getElementById('win-modal-container');
+const chancesDisplay = document.querySelector('h2');
+const timerDisplay = document.getElementById('counter');
+const introModal = document.getElementById('intro-modal-container');
+const loseModal = document.getElementById('lose-modal-container');
+const winModal = document.getElementById('win-modal-container');
   
-  document.querySelector('main').addEventListener('click', handleChoice);
-  document.getElementById('replay').addEventListener('click', replayGame);
-  document.getElementById('play-again').addEventListener('click', replayGame);
+document.querySelector('main').addEventListener('click', handleChoice);
+document.getElementById('replay').addEventListener('click', replayGame);
+document.getElementById('play-again').addEventListener('click', replayGame);
 document.getElementById('ready').addEventListener("click", function() {
     startGame();
     document.getElementById('game-screen').style.display = 'block';
@@ -45,8 +50,8 @@ function play() {
   cards = getShuffledCards();
   selectedCard = null;
   ignoreClicks = false;
-  seconds = 60;
-  chances = 20;
+  seconds = 90;
+  chances = 30;
   matches = 0;
   winner = null;
   render();
@@ -55,17 +60,17 @@ function play() {
   function render() {
   cards.forEach(function (card, idx) {
     const imgEl = document.getElementById(idx);
-    const src = (card.matched || card === selectedCard) ? card.img : CARD_BACK;
+    const src = (card.matched || card === selectedCard) ? card.img : objectCover;
     imgEl.src = src;
   });
-  chancesEl.innerHTML = `chances: ${chances}/20`;
-  timerEl.innerHTML = `0:${seconds}`;
+  chancesDisplay.innerHTML = `chances: ${chances}/20`;
+  timerDisplay.innerHTML = `0:${seconds}`;
   }
   
   function getShuffledCards() {
   let tempCards = [];
   let cards = [];
-  for (let card of SOURCE_CARDS) {
+  for (let card of artObjects) {
     tempCards.push({ ...card }, { ...card });
   }
   while (tempCards.length) {
@@ -157,8 +162,8 @@ function startTimer() {
     winSound.play();
     selectedCard = null;
     winModal.classList.add('show');
-    timerEl.style.visibility = 'hidden';
-    chancesEl.style.visibility = 'hidden';
+    timerDisplay.style.visibility = 'hidden';
+    chancesDisplay.style.visibility = 'hidden';
     document.getElementById('game-logo').style.display = 'block';
     document.querySelector('h3').style.display = 'block';
   }
@@ -168,8 +173,8 @@ function startTimer() {
     resetTimer();
     startGame();
     loseModal.classList.remove('show');
-    timerEl.style.visibility = 'visible';
-    chancesEl.style.visibility = 'visible';
+    timerDisplay.style.visibility = 'visible';
+    chancesDisplay.style.visibility = 'visible';
     document.getElementById('game-logo').style.display = 'block';
     document.querySelector('h3').style.display = 'block';
     document.getElementById('stats').style.display = 'block';
@@ -182,8 +187,8 @@ function startTimer() {
     loseSound.play();
     selectedCard = null;
     loseModal.classList.add('show');
-    timerEl.style.visibility = 'hidden';
-    chancesEl.style.visibility = 'hidden';
+    timerDisplay.style.visibility = 'hidden';
+    chancesDisplay.style.visibility = 'hidden';
     document.getElementById('game-logo').style.display = 'none';
     document.querySelector('h3').style.display = 'none';
     document.getElementById('stats').style.display = 'none';
